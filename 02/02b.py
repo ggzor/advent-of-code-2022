c7e8b0b1c31b70eefcec54699978e496640e8ed5
ggzor <30713864+ggzor@users.noreply.github.com>
2022-12-01 23:18:49 -0600

    python: Add day 02 solution

---
+++ 02/02b.py
@@ -0,0 +1,19 @@
+from utils import *
+
+scores = {
+    "A X": 3 + 0,
+    "A Y": 1 + 3,
+    "A Z": 2 + 6,
+    "B X": 1 + 0,
+    "B Y": 2 + 3,
+    "B Z": 3 + 6,
+    "C X": 2 + 0,
+    "C Y": 3 + 3,
+    "C Z": 1 + 6,
+}
+
+s = 0
+for l in input_lines():
+    s += scores[l]
+
+print(s)
