4cc1abcabc085dece11880290068da8a759c1e5e
ggzor <30713864+ggzor@users.noreply.github.com>
2022-12-03 23:17:49 -0600

    python: Add day 04 solution

---
+++ 04/04a.py
@@ -0,0 +1,15 @@
+from utils import *
+
+total = 0
+for l in input_lines():
+    x, y = l.split(",")
+    a, b = map(int, x.split("-"))
+    c, d = map(int, y.split("-"))
+
+    s1 = set(range(a, b + 1))
+    s2 = set(range(c, d + 1))
+
+    if s1 <= s2 or s2 <= s1:
+        total += 1
+
+print(total)
